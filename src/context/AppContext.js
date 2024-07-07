3a5404a145965413b8d6996b38780bb3c73741f0
WaldenBingham <sebastian@amler.de>
2024-07-07 21:35:04 +0200

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -30,6 +30,7 @@ export const AppReducer = (state, action) => {
                     ...state
                 }
             }
+
             case 'RED_EXPENSE':
                 const red_expenses = state.expenses.map((currentExp)=> {
                     if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
@@ -57,6 +58,7 @@ export const AppReducer = (state, action) => {
                 ...state,
                 budget
             };
+        
         case 'SET_BUDGET':
             action.type = "DONE";
             state.budget = action.payload;
@@ -70,6 +72,22 @@ export const AppReducer = (state, action) => {
             return {
                 ...state
             }
+        
+            case 'DEC_EXPENSE':
+                const dec_expenses = state.expenses.map((currentExp) => {
+                    if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
+                        currentExp.cost = currentExp.cost - action.payload.cost;
+                        budget = state.budget + action.payload.cost;
+                    }
+                    return currentExp;
+                });
+                action.type = "DONE";
+                return {
+                    ...state,
+                    expenses: [...dec_expenses],
+                    budget
+                };
+            
 
         default:
             return state;
